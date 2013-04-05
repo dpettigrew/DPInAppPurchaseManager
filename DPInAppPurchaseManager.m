5c9c5535131634989b32006843f70b2d2d2e496a
David Pettigrew <dpettigrew@burnsidedigital.com>
2013-04-05 11:28:37 -0700

    first version

---
+++ DPInAppPurchaseManager.m
@@ -0,0 +1,219 @@
+//
+//  DPInAppPurchaseManager.m
+//
+//  Copyright (c) David Pettigrew. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//      * Neither the name of the David Pettigrew nor the
+//        names of its contributors may be used to endorse or promote products
+//        derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "DPInAppPurchaseManager.h"
+
+@implementation DPInAppPurchaseManager
+
+CWL_SYNTHESIZE_SINGLETON_FOR_CLASS(DPInAppPurchaseManager)
+
+- (void)requestUpgradeProductData
+{
+    NSSet *productIdentifiers = [NSSet setWithObject:self.productID ];
+    productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:productIdentifiers];
+    productsRequest.delegate = self;
+    _isGettingProducts = YES;
+    [productsRequest start];
+}
+
+#pragma mark -
+#pragma mark SKProductsRequestDelegate methods
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    _isGettingProducts = NO;
+    _gotProducts = YES;
+    NSArray *products = response.products;
+    self.upgradeProduct = [products count] == 1 ? products[0] : nil;
+    if (self.upgradeProduct)
+    {
+        NSLog(@"Product title: %@" , self.upgradeProduct.localizedTitle);
+        NSLog(@"Product description: %@" , self.upgradeProduct.localizedDescription);
+        NSLog(@"Product price: %@" , self.upgradeProduct.price);
+        NSLog(@"Product id: %@" , self.upgradeProduct.productIdentifier);
+        [[NSNotificationCenter defaultCenter] postNotificationName:kDPInAppPurchaseManagerProductsFetchedNotification object:self userInfo:@{@"upgradeProduct": self.upgradeProduct}];
+    }
+    
+    for (NSString *invalidProductId in response.invalidProductIdentifiers)
+    {
+        NSLog(@"Invalid product id: %@" , invalidProductId);
+    }
+}
+
+//
+// call this method once on startup
+//
+- (void)loadStore
+{
+    // restarts any purchases if they were interrupted last time the app was open
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    
+    // get the product description (defined in early sections)
+    [self requestUpgradeProductData];
+}
+
+//
+// call this before making a purchase
+//
+- (BOOL)canMakePurchases
+{
+    return [SKPaymentQueue canMakePayments];
+}
+
+//
+// kick off the upgrade transaction
+//
+- (void)purchaseUpgrade
+{
+    if (self.upgradeProduct) {
+        SKPayment *payment = [SKPayment paymentWithProduct:self.upgradeProduct];
+        [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+        [[SKPaymentQueue defaultQueue] addPayment:payment];
+    }
+    else {
+        NSLog(@"No upgrade product available");
+    }
+}
+
+#pragma -
+#pragma Purchase helpers
+
+//
+// saves a record of the transaction by storing the receipt to disk
+//
+- (void)recordTransaction:(SKPaymentTransaction *)transaction
+{
+    if ([transaction.payment.productIdentifier isEqualToString:self.productID])
+    {
+        // save the transaction receipt to disk
+        [[NSUserDefaults standardUserDefaults] setValue:transaction.transactionReceipt forKey:@"upgradeTransactionReceipt" ];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
+//
+// enable pro features
+//
+- (void)provideContent:(NSString *)productId
+{
+    if ([productId isEqualToString:self.productID])
+    {
+        // enable the pro features
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kUpgradePurchased];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        [[NSNotificationCenter defaultCenter] postNotificationName:kInAppPurchaseUpgradePurchasedNotification object:self userInfo:nil];
+    }
+}
+
+//
+// removes the transaction from the queue and posts a notification with the transaction result
+//
+- (void)finishTransaction:(SKPaymentTransaction *)transaction wasSuccessful:(BOOL)wasSuccessful
+{
+    // remove the transaction from the payment queue.
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+    
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:transaction, @"transaction" , nil];
+    if (wasSuccessful)
+    {
+        // send out a notification that we’ve finished the transaction
+        [[NSNotificationCenter defaultCenter] postNotificationName:kDPInAppPurchaseManagerTransactionSucceededNotification object:self userInfo:userInfo];
+    }
+    else
+    {
+        // send out a notification for the failed transaction
+        [[NSNotificationCenter defaultCenter] postNotificationName:kDPInAppPurchaseManagerTransactionFailedNotification object:self userInfo:userInfo];
+    }
+}
+
+//
+// called when the transaction was successful
+//
+- (void)completeTransaction:(SKPaymentTransaction *)transaction
+{
+    [self recordTransaction:transaction];
+    [self provideContent:transaction.payment.productIdentifier];
+    [self finishTransaction:transaction wasSuccessful:YES];
+}
+
+//
+// called when a transaction has been restored and and successfully completed
+//
+- (void)restoreTransaction:(SKPaymentTransaction *)transaction
+{
+    [self recordTransaction:transaction.originalTransaction];
+    [self provideContent:transaction.originalTransaction.payment.productIdentifier];
+    [self finishTransaction:transaction wasSuccessful:YES];
+}
+
+//
+// called when a transaction has failed
+//
+- (void)failedTransaction:(SKPaymentTransaction *)transaction
+{
+    if (transaction.error.code != SKErrorPaymentCancelled)
+    {
+        // error!
+        [self finishTransaction:transaction wasSuccessful:NO];
+    }
+    else
+    {
+        // this is fine, the user just cancelled, so don’t notify
+        [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+    }
+}
+
+#pragma mark -
+#pragma mark SKPaymentTransactionObserver methods
+
+//
+// called when the transaction status is updated
+//
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions
+{
+    for (SKPaymentTransaction *transaction in transactions)
+    {
+        switch (transaction.transactionState)
+        {
+            case SKPaymentTransactionStatePurchased:
+                [self completeTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                [self failedTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateRestored:
+                [self restoreTransaction:transaction];
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+@end
